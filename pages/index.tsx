a561bc7ad796974a209a6f423e127331c3acbd9b
michaelchristwin <kelechukwuchristwin@gmail.com>
2023-02-26 08:06:45 +0100

    Added search

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,24 @@
+import React from "react";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+
 function Home() {
   return (
     <main className="w-full">
-      <h1 className="text-center block">Bazaar</h1>
+      <div className="pt-5">
+        <p className="block ml-2 text-blue-700 text-[1.23rem] font-['Helvetica'] font-[700]">
+          BAZAAR
+        </p>
+      </div>
+      <div className="relative">
+        <input
+          type="text"
+          name=""
+          className="rounded-[0.7rem] ml-1 block text-black h-10 pl-8 border"
+          id="Search"
+          placeholder={`Search`}
+        />
+        <SearchOutlinedIcon className="absolute top-2 left-2" />
+      </div>
     </main>
   );
 }
