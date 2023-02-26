ac403ade5d9ff1f87234b36a45de2e63d520e6b9
michaelchristwin <kelechukwuchristwin@gmail.com>
2023-02-26 09:41:05 +0100

    Added search

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,27 @@
 import React from "react";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 
 function Home() {
   return (
     <main className="w-full">
-      <div className="pt-5">
+      <div className="pt-5 flex justify-between">
         <p className="block ml-2 text-blue-700 text-[1.23rem] font-['Helvetica'] font-[700]">
           BAZAAR
         </p>
+        <div>
+          <ShoppingCartOutlinedIcon className="mr-3" />
+        </div>
       </div>
-      <div className="relative">
+      <div className="relative block w-full mt-3">
         <input
           type="text"
           name=""
-          className="rounded-[0.7rem] ml-1 block text-black h-10 pl-8 border"
+          className="rounded-[0.7rem] lg:ml-1 block text-black h-10 pl-8 border w-[90%] mx-auto"
           id="Search"
           placeholder={`Search`}
         />
-        <SearchOutlinedIcon className="absolute top-2 left-2" />
+        <SearchOutlinedIcon className="absolute top-2 left-6 lg:left-2" />
       </div>
     </main>
   );
