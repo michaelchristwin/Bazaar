1648fa6a8041ebbf71ee7f91779acea4b9076057
michaelchristwin <kelechukwuchristwin@gmail.com>
2023-02-27 10:50:18 +0100

    Re

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import Person2OutlinedIcon from "@mui/icons-material/Person2Outlined";
 
 function Home() {
   return (
@@ -9,20 +10,28 @@ function Home() {
         <p className="block ml-2 text-blue-700 text-[1.23rem] font-['Helvetica'] font-[700]">
           BAZAAR
         </p>
-        <div>
-          <ShoppingCartOutlinedIcon className="mr-3" />
+        <div className="flex">
+          <p>
+            <Person2OutlinedIcon className="mr-4" />
+          </p>
+          <p>
+            <ShoppingCartOutlinedIcon className="mr-3" />
+          </p>
         </div>
       </div>
-      <div className="relative block w-full mt-3">
+      <fieldset className="relative w-full mt-3 flex">
         <input
           type="text"
           name=""
-          className="rounded-[0.7rem] lg:ml-1 block text-black h-10 pl-8 border w-[90%] mx-auto"
+          className="rounded-[0.7rem] lg:ml-1 block text-black h-10 pl-8 border-2 w-[75%] ml-2"
           id="Search"
           placeholder={`Search`}
         />
-        <SearchOutlinedIcon className="absolute top-2 left-6 lg:left-2" />
-      </div>
+        <SearchOutlinedIcon className="absolute top-2 left-4 lg:left-2" />
+        <button className=" bg-blue-700 text-white px-3 rounded ml-2 hover:bg-blue-600">
+          Search
+        </button>
+      </fieldset>
     </main>
   );
 }
