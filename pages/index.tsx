28ab0c6ab5f5d2561396d7269e19a4b31eabb975
michaelchristwin <kelechukwuchristwin@gmail.com>
2023-03-02 05:43:56 +0100

    Added offcanvas

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,38 +2,50 @@ import React, { useState } from "react";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import Person2OutlinedIcon from "@mui/icons-material/Person2Outlined";
-import Image from "next/image";
 import "bootstrap/dist/css/bootstrap.min.css";
+import MenuIcon from "@mui/icons-material/Menu";
+import Offcanvas from "react-bootstrap/Offcanvas";
 
-function Home() {
+function Home(): JSX.Element {
   const [open, setOpen] = useState<boolean>(false);
+  const handleClose = () => setOpen(false);
+  const handleShow = () => setOpen(true);
+
   return (
     <main className="">
-      <div className="pt-5 d-flex justify-content-between">
-        <p className="d-block ms-2">BAZAAR</p>
+      <div className="pt-3 d-flex justify-content-between">
+        <button className="btn menu border ms-2" onClick={handleShow}>
+          <MenuIcon />
+        </button>
+        <p className="d-block ms-2 brand">BAZAAR</p>
         <div className="d-flex">
           <p>
-            <Person2OutlinedIcon className="me-4" />
+            <Person2OutlinedIcon className="me-4 person" />
           </p>
           <p>
-            <ShoppingCartOutlinedIcon className="mr-3" />
+            <ShoppingCartOutlinedIcon className="mr-3 cart" />
           </p>
         </div>
       </div>
-      <fieldset className="position-relative mt-3 d-flex">
+      <fieldset className="position-relative d-block">
         <input
           type="text"
           name=""
-          className="form-control form ms-2 text-center"
+          className="form-control form mx-auto text-center"
           id="Search"
           placeholder={`Search`}
         />
         <SearchOutlinedIcon className="search-icon" />
-        <button className="btn btn-primary ms-1">Search</button>
       </fieldset>
-      <button className="btn btn-primary p-2">
-        <Image src={"/menu.png"} width={25} height={20} alt="wi" />
-      </button>
+      <Offcanvas show={open} onHide={handleClose} id="offcanvas">
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          Some text as placeholder. In real life you can have the elements you
+          have chosen. Like, text, images, lists, etc.
+        </Offcanvas.Body>
+      </Offcanvas>
     </main>
   );
 }
