9a62edb29472bc9ae4e740eb151ad15f9d8424ee
michaelchristwin <kelechukwuchristwin@gmail.com>
2023-03-15 16:43:13 +0100

    I hate css:(

--- pages/home.tsx
+++ pages/home.tsx
@@ -11,31 +11,33 @@ import Link from "next/link";
 
 function Home(): JSX.Element {
   return (
-    <main className=" relative">
-      <nav className="bg-green-600 flex justify-around text-white h-10 align-middle pt-3 fixed w-full bottom-0">
-        <Link href={"/"}>
-          <FontAwesomeIcon icon={faBars} style={{ fontSize: "1rem" }} />
-        </Link>
-        <Link href={"/"}>
-          <FontAwesomeIcon icon={faHouse} style={{ fontSize: "1.3rem" }} />
-        </Link>
-        <Link href={"/"}>
-          <FontAwesomeIcon
-            icon={faCartShopping}
-            style={{ fontSize: "1.3rem" }}
-          />
-        </Link>
-        <Link href={"/"}>
-          <FontAwesomeIcon icon={faUser} style={{ fontSize: "1.3rem" }} />
-        </Link>
-        <Link href={"/"}>
-          <FontAwesomeIcon icon={faComment} style={{ fontSize: "1.3rem" }} />
-        </Link>
-      </nav>
+    <main className="h-full min-h-screen">
+      <div className="horror">
+        <nav className="bg-green-600 flex justify-around text-white h-10 pt-3 w-full horror">
+          <Link href={"/"}>
+            <FontAwesomeIcon icon={faBars} style={{ fontSize: "1rem" }} />
+          </Link>
+          <Link href={"/"}>
+            <FontAwesomeIcon icon={faHouse} style={{ fontSize: "1.3rem" }} />
+          </Link>
+          <Link href={"/"}>
+            <FontAwesomeIcon
+              icon={faCartShopping}
+              style={{ fontSize: "1.3rem" }}
+            />
+          </Link>
+          <Link href={"/"}>
+            <FontAwesomeIcon icon={faUser} style={{ fontSize: "1.3rem" }} />
+          </Link>
+          <Link href={"/"}>
+            <FontAwesomeIcon icon={faComment} style={{ fontSize: "1.3rem" }} />
+          </Link>
+        </nav>
+      </div>
+
       <div className="relative">
         <input
           type="text"
-          id=""
           placeholder="Search"
           className="pl-7 rounded-[20px] h-[37px] block w-[80vw] mt-2 border border-black ml-3"
         />
