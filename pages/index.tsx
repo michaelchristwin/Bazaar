9bddc195c358508890e62ad56a49dac34ed43a5f
michaelchristwin <kelechukwuchristwin@gmail.com>
2023-04-13 12:11:50 +0100

    999

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,6 @@
 import Image from "next/image";
 
 function Home() {
-  let slideIndex = 1;
-  showSlides(slideIndex);
-
-  function plusSlides(n: number) {
-    showSlides((slideIndex += n));
-  }
-
-  function currentSlide(n: number) {
-    showSlides((slideIndex = n));
-  }
-
-  function showSlides(n: number) {
-    const slides = document.querySelectorAll(".carousel-item");
-    const dots = document.querySelectorAll(".dot");
-
-    if (n > slides.length) {
-      slideIndex = 1;
-    }
-    if (n < 1) {
-      slideIndex = slides.length;
-    }
-    for (let i = 0; i < slides.length; i++) {
-      slides[i].classList.add("hidden");
-      slides[i].classList.remove("active:block");
-    }
-    for (let i = 0; i < dots.length; i++) {
-      dots[i].classList.remove("active");
-    }
-    slides[slideIndex - 1].classList.remove("hidden");
-    slides[slideIndex - 1].classList.add("active:block");
-    dots[slideIndex - 1].classList.add("active");
-  }
-
   return (
     <main className="bg-[#fafafa]">
       <section>
@@ -90,118 +57,13 @@ function Home() {
           </aside>
         </div>
         <div>
-          <div className="relative">
-            <div className="relative w-full overflow-hidden">
-              <div className="active:block relative float-left hidden carousel-item">
-                <Image
-                  src={`/fresh.webp`}
-                  width={690}
-                  height={270}
-                  alt="Fresh Finds"
-                  className="rounded"
-                />
-              </div>
-              <div className="active:block relative float-left hidden carousel-item">
-                <Image
-                  src={`/mom.webp`}
-                  width={690}
-                  height={270}
-                  alt="Fresh Finds"
-                  className="rounded"
-                />
-              </div>
-              <div className="active:block relative float-left hidden carousel-item">
-                <Image
-                  src={`/toy.webp`}
-                  width={690}
-                  height={270}
-                  alt="Fresh Finds"
-                  className="rounded"
-                />
-              </div>
-              <div className="active:block relative float-left hidden carousel-item">
-                <Image
-                  src={`/note12.webp`}
-                  width={690}
-                  height={270}
-                  alt="Fresh Finds"
-                  className="rounded"
-                />
-              </div>
-            </div>
-            <a
-              href="#carouselExampleIndicators"
-              role="button"
-              className="carousel-control-prev"
-            >
-              <span
-                className="carousel-control-prev-icon"
-                aria-hidden="true"
-              ></span>
-              <span className="sr-only">Previous</span>
-            </a>
-            <a
-              className="carousel-control-next"
-              href="#carouselExampleIndicators"
-              role="button"
-              data-slide="next"
-            >
-              <span
-                className="carousel-control-next-icon"
-                aria-hidden="true"
-              ></span>
-              <span className="sr-only">Next</span>
-            </a>
-          </div>
-          <div className="carousel relative overflow-hidden">
-            <div className="carousel-inner">
-              <div className="carousel-item absolute w-full">
-                <Image
-                  src={`/fresh.webp`}
-                  width={690}
-                  height={270}
-                  alt="Fresh Finds"
-                  className="rounded"
-                />
-              </div>
-              <div className="carousel-item absolute w-full">
-                <Image
-                  src={`/mom.webp`}
-                  width={690}
-                  height={270}
-                  alt="Fresh Finds"
-                  className="rounded"
-                />
-              </div>
-              <div className="carousel-item absolute w-full">
-                <Image
-                  src={`/toy.webp`}
-                  width={690}
-                  height={270}
-                  alt="Fresh Finds"
-                  className="rounded"
-                />
-              </div>
-              <Image
-                src={`/toy.webp`}
-                width={690}
-                height={270}
-                alt="Fresh Finds"
-                className="rounded"
-              />
-            </div>
-          </div>
-          <button className="carousel-prev absolute top-1/2 left-0 transform -translate-y-1/2 bg-black bg-opacity-50 text-white px-3 py-2 rounded-l-md">
-            Previous
-          </button>
-          <button className="carousel-next absolute top-1/2 right-0 transform -translate-y-1/2 bg-black bg-opacity-50 text-white px-3 py-2 rounded-r-md">
-            Next
-          </button>
-          <div className="carousel-dots absolute bottom-0 left-0 right-0 flex justify-center mb-4">
-            <span className="carousel-dot inline-block h-3 w-3 rounded-full bg-gray-400 mx-2"></span>
-            <span className="carousel-dot inline-block h-3 w-3 rounded-full bg-gray-400 mx-2"></span>
-            <span className="carousel-dot inline-block h-3 w-3 rounded-full bg-gray-400 mx-2"></span>
-          </div>
+          <Image
+            src={`/mom.webp`}
+            width={690}
+            height={270}
+            alt="Mom Deals"
+            className="rounded"
+          />
         </div>
       </section>
     </main>
